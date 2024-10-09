6be13d43bab724fdb7d9e276253f6bf8ce70357e
rehmanli1470ww <144922823+rehmanli1470ww@users.noreply.github.com>
2024-10-09 18:01:12 +0400

    Add project files.

---
+++ SocialNetwork.WebUI/Migrations/20241007073719_Message.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetwork.WebUI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Message : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReceiverId",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SenderId",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReceiverId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "SenderId",
+                table: "Messages");
+        }
+    }
+}
